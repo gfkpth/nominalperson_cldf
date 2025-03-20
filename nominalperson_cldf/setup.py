234f006ed94b336758371bc2fbc06bb9d37ebee6
gfkpth <groeg3@web.de>
2025-03-20 12:31:06 +0100

    initial commit

---
+++ nominalperson_cldf/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_nominalperson_cldf',
+    py_modules=['cldfbench_nominalperson_cldf'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'nominalperson_cldf=cldfbench_nominalperson_cldf:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
