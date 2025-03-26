ed004cb098f1ffa547d2e06ec8ae7297f4d9d777
gfkpth <groeg3@web.de>
2025-03-26 22:36:07 +0100

    experimenting with makecldf implementation

--- cldfbench_nominalperson_cldf.py
+++ cldfbench_nominalperson_cldf.py
@@ -25,20 +25,58 @@ class Dataset(BaseDataset):
     def cmd_makecldf(self, args):
         # Extend the schema:
         args.writer.cldf.add_component('LanguageTable')
+        args.writer.cldf.add_columns('LanguageTable','Walscode')    # add non-standard column
+        
+        args.writer.cldf.add_component('CodeTable')
+        args.writer.cldf.add_component('ParameterTable')
+        args.writer.cldf.add_component('ValueTable')
         args.writer.cldf.add_component('ExampleTable')
 
-        for lid, rows in itertools.groupby(
-            sorted(
-                self.raw_dir.read_csv('examples.csv', dicts=True),
-                key=lambda row: (row['Language_ID'], int(row['ID']))),
-            lambda row: row['Language_ID']
-        ):
-            rows = list(rows)
-            args.writer.objects['LanguageTable'].append(dict(
-                ID=lid,
-                Name=rows[0]['Language_Name'],
-            ))
-            for row in rows:
+        # Load data from grammarchecks
+        
+        lang_id=0
+        val_id=0
+        for row in self.raw_dir.read_csv('grammarchecks.csv',dicts=True):
+            if row['include']=='y':
+                args.writer.objects['LanguageTable'].append({
+                    'ID': row['glottocode'],
+                    'Name': row['lang_name'] ,
+                    'Macroarea': row['area_glottolog'],
+                    'Latitude': row['lat'],
+                    'Longitude': row['lon'],
+                    'Glottocode': row['glottocode'],
+                    'ISO639P3code': row['iso639_3'],
+                    'Walscode': row['walscode']
+                })
+                args.writer.objects['ValueTable'].append({
+                    'ID': val_id,
+                    'Language_ID': row['glottocode'],
+                    'Parameter_ID': 'APC Order',
+                    'Value': row['apc_order']
+                })
+                val_id+=1
+                args.writer.objects['ValueTable'].append({
+                    'ID': val_id,
+                    'Language_ID': row['glottocode'],
+                    'Parameter_ID': 'Constituent Order',
+                    'Value': row['constituent_order']
+                })
+                val_id+=1
+            
+            
+ #       for lid, rows in itertools.groupby(
+ #           sorted(
+ #               self.raw_dir.read_csv('examples.csv', dicts=True),
+ #               key=lambda row: (row['Language_ID'], int(row['ID']))),
+ #           lambda row: row['Language_ID']
+ #       ):
+ #           rows = list(rows)
+ #           args.writer.objects['LanguageTable'].append(dict(
+ #               ID=lid,
+ #               Name=rows[0]['Language_Name'],
+ #           ))
+ #           for row in rows:
+            for row in self.raw_dir.read_csv('examples.csv',dicts=True):
                 args.writer.objects['ExampleTable'].append({
                     'ID': row['ID'],
                     'Language_ID': row['Language_ID'],
@@ -51,3 +89,5 @@ class Dataset(BaseDataset):
                     'Source': row['Source'],
                     'Comments': row['Comment']
                 })
+        
+
